7e817e57ff7266247d38492a089b99ce443d3307
Andrew Lam <andrewlam2193@gmail.com>
2023-02-18 01:18:29 -0800

    Added task prompts to best practices

--- CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskA.java
+++ CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskA.java
@@ -1,11 +1,22 @@
 /** Author: Andrew Lam
  *  Course: CS 2450
- *  Objective: Assignment 1
+ *  Assignment: Assignment 1
  *  Due Date: 02.27.2023
+ *  Objective:  Tip, Tax, and Total - Create a JavaFX application that lets the user enter the food charge for a meal
+ *              at a restaurant. When a button is clicked, the application should calculate and display the amount of
+ *              an 18 percent tip on the total food charge, 7 percent sales tax, and the total of all three amounts.
  */
 
 package com.example.cs2450_assignment1;
 
-public class TaskA {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class TaskA extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
--- CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskB.java
+++ CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskB.java
@@ -1,10 +1,22 @@
 /** Author: Andrew Lam
  *  Course: CS 2450
- *  Objective: Assignment 1
+ *  Assignment: Assignment 1
  *  Due Date: 02.27.2023
+ *  Objective:  Dice Simulator - Create a JavaFX application that simulates rolling a pair of dice. When the user clicks
+ *              a button, the application should generate two random numbers, each in the range of 1 through 6, to
+ *              represent the value of the dice. Use ImageView controls to display the dice. (In the Dice.zip file
+ *              on canvas, you will find six images named Die1.png, Die2.png, Die3.png, Die4.png, Die5.png,
+ *              and Die6.png that you can use in the ImageView controls.)
  */
 
 package com.example.cs2450_assignment1;
 
-public class TaskB {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TaskB extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
