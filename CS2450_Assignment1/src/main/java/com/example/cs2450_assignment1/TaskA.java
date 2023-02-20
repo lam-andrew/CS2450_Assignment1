423204d192482ffcd21092aa23c85dfeb8d5d314
Andrew Lam <andrewlam2193@gmail.com>
2023-02-19 21:51:14 -0800

    Aligned labels and text fields and added button functionality

--- CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskA.java
+++ CS2450_Assignment1/src/main/java/com/example/cs2450_assignment1/TaskA.java
@@ -10,35 +10,78 @@
 package com.example.cs2450_assignment1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TaskA extends Application {
+    // Initialize constructs for the charge of food
+    Label foodChargeLabel = new Label("Enter meal cost: ");
+    TextField foodCharge = new TextField();
+
+    // Initialize constructs for 7 percent sales tax
+    Label taxLabel = new Label("7 percent sales tax: ");
+    TextField tax = new TextField();
+
+    // Initialize constructs for 18 percent tip
+    Label tipLabel = new Label("18 percent tip: ");
+    TextField tip = new TextField();
+
+    // Initialize constructs for total of food charge, tax, and tip
+    Label totalLabel = new Label("Total: ");
+    TextField total = new TextField();
+
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Initialize constructs for the charge of food
-        Label foodChargeLabel = new Label("Enter meal cost: ");
-        TextField foodCharge = new TextField();
+        // Set labels non-editable
+        tax.setEditable(false);
+        tip.setEditable(false);
+        total.setEditable(false);
 
-        // Initialize constructs for 7 percent sales tax
-        
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.add(foodChargeLabel, 0, 1);
+        gridPane.add(foodCharge, 1, 1);
+        gridPane.add(taxLabel, 2, 1);
+        gridPane.add(tax, 3, 1);
+        gridPane.add(tipLabel, 2, 2);
+        gridPane.add(tip, 3, 2);
+        gridPane.add(totalLabel, 2, 3);
+        gridPane.add(total, 3, 3);
 
-        // Initialize constructs for 18 percent tip
-
-
+        Button calcButton = new Button("Calculate");
+        calcButton.setOnAction(new calcClickHandler());
 
         // Initialize VBox to hold all other controls
-        VBox vBox = new VBox();
+        VBox vBox = new VBox(gridPane, calcButton);
+        vBox.setPadding(new Insets(50));
+        vBox.setAlignment(Pos.CENTER);
         // Initialize Scene with VBox as the root
-        Scene myScene = new Scene(vBox, 600, 400);
+        Scene myScene = new Scene(vBox, 700, 200);
 
         // Stage initialization
         stage.setScene(myScene);
         stage.setTitle("Task A - Tip, Tax, and Total ");
         stage.show();
     }
+
+    // Handler class for calcButton
+    class calcClickHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            total.setText("hi");
+        }
+    }
+
 }
